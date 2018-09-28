fdb52190a1ab325f5fe458fd7feff1ca6c341010
ja6son <3062573144@qq.com>
2018-09-28 17:04:26 +0800

    模块对象构造完

---
+++ 旧书回流/app/models.py
@@ -0,0 +1,32 @@
+from exts import db
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer,primary_key = True,autoincrement = True)
+    user_name = db.Column(db.String(30),nullable = False)
+    password = db.Column(db.String(50),nullable = False)
+    telephone = db.Column(db.String(20),nullable = False)
+    qq = db.Column(db.String(20),nullable = False)
+    sex = db.Column(db.String(10),nullable = False)
+    sign = db.Column(db.String(100),nullable = False)
+    avatar = db.Column(db.String(128),default = "default_avatar.jpg")
+    avatar_id = db.Column(db.Integer,default = 1)
+    books = db.relationship('Book',backref = 'user')
+    comments = db.relationship('Comment',backref = 'user')
+
+class Book(db.Model):
+    __tablename__ = 'book'
+    id = db.Column(db.Integer,primary_key = True,autoincrement = True)
+    book_name = db.Column(db.String(50),nullable = False)
+    content = db.Column(db.Text,nullable = False)
+    book_image = db.Column(db.String(128),default = "default_book_image.jpg")
+    book_image_id = db.Column(db.Integer,default = 1)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    comments = db.relationship('Comment',backref = 'book')
+
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer,primary_key = True,autoincrement = True)
+    content = db.Column(db.Text,nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    book_id = db.Column(db.Integer,db.ForeignKey('book.id'))
--- 旧书回流/app/run.py
+++ 旧书回流/app/run.py
@@ -1,14 +1,23 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import config
+from exts import db
+
 
 app = Flask(__name__)
 app.config.from_object('config')
+db.init_app(app)
 
 @app.route('/')
 @app.route('/index/')
 def index():
     return render_template('base.html')
 
+@app.route('/login/',methods = ['POST','GET'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else :
+        pass
 
 if __name__ == '__main__':
     app.run()
