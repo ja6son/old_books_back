9039844ce2a144c63e98367d56eb7a2fb0479d56
ja6son <3062573144@qq.com>
2018-10-02 10:23:32 +0800

    注册页面完成

--- 旧书回流/app/run.py
+++ 旧书回流/app/run.py
@@ -1,6 +1,7 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect,url_for,flash
 import config
 from exts import db
+from models import User,Book,Comment
 
 
 app = Flask(__name__)
@@ -12,12 +13,37 @@ db.init_app(app)
 def index():
     return render_template('base.html')
 
-@app.route('/login/',methods = ['POST','GET'])
+@app.route('/login/',methods = ['POST','GET'])  
 def login():
     if request.method == 'GET':
         return render_template('login.html')
     else :
         pass
+
+@app.route('/regist/',methods = ['POST','GET'])
+def regist():
+    if request.method == 'GET':
+        return render_template('regist.html')
+    else :
+        user_name = request.form.get('user_name')
+        password = request.form.get('password')
+        passwords = request.form.get('passwords')
+        if User.query.filter(User.user_name == user_name).first() :
+            flash('该用户已存在')
+            return redirect(url_for('regist'))
+        elif password != passwords:
+            flash('两次密码不同')
+            return redirect(url_for('regist'))
+        else :
+            telephone = request.form.get('telephone')
+            sex = request.form.get('sex')
+            qq = request.form.get('qq')
+            sign = request.form.get('sign')
+            user = User(user_name = user_name,password = password,
+                        telephone = telephone,qq = qq,sex = sex ,sign = sign)
+            db.session.add(user)
+            db.session.commit()
+            return 'OK'
 
 if __name__ == '__main__':
     app.run()
