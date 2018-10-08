2e2f430df1bd7671da2eb518b3746bacdcf7bfc0
ja6son <3062573144@qq.com>
2018-10-08 11:34:56 +0800

    delete

--- 旧书回流/app/config.py
+++
@@ -1,7 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///'+ os.path.join(basedir,'dbapp.db')
-
-SQLALCHEMY_TRACK_MODIFICATIONS=False
-SECRET_KEY = 'mysecret666'
-DEBUG = True
