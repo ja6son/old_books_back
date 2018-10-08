1129d22ef6efca742a627e9319373579041d36b9
ja6son <3062573144@qq.com>
2018-10-08 11:28:59 +0800

    new name

---
+++ book_back/app/manages.py
@@ -0,0 +1,14 @@
+from flask_script import Manager
+from flask_migrate import MigrateCommand,Migrate
+from run import app
+from exts import db
+from models import User,Book,Comment
+
+manage = Manager(app)
+
+migrate = Migrate(app,db)
+
+manage.add_command('db',MigrateCommand)
+
+if __name__ == '__main__':
+    manage.run()
